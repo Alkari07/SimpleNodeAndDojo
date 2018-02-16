43012b1f1900be6c25f21b242a4fcaac3de8ab29
Rob Santos <rob9575@esri.com>
2018-02-16 14:41:09 -0600

    Setup basic node express web server

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+//viewed at :8080
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname +'/static/index.html'));
+});
+
+app.listen(8080);
